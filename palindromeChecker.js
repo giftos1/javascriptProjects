527d845c9dc719a3dcd8ac39f8559367e5e05a4e
Giftos1 <giftsydney676@gmail.com>
2023-07-08 03:46:12 +1000

    return true if palindrome

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -10,7 +10,26 @@ into the same case (lower or upper case) in order to check for palindromes.
 We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.*/
 
 function palindrome(str) {
-    return str;
+    const strRegex = /[\W_]/g;
+    const reversedArr = str.replace(strRegex, "").split("").reverse();
+    const reversedStr = reversedArr.join("").toLowerCase();
+    if(strRegex.test(str)) {
+        str = str.replace(strRegex, "").toLowerCase();
+    }
+    // console.log(reversedStr)
+    return str === reversedStr;
 }
 
-console.log(palindrome("eye"))
+//tests
+console.log(palindrome("eye"));
+console.log(palindrome("_eye") );
+console.log(palindrome("race car"));
+console.log(palindrome("not a palindrome"));
+console.log(palindrome("A man, a plan, a canal. Panama"));
+console.log(palindrome("never odd or even"));
+console.log(palindrome("nope"));
+console.log(palindrome("almostomla"));
+console.log(palindrome("My age is 0, 0 si ega ym."));
+console.log(palindrome("1 eye for of 1 eye."));
+console.log(palindrome("0_0 (: /-\ :) 0-0"));
+console.log(palindrome("five|\_/|four"));
