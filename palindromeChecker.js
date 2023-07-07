f27f14797aef8e6f7e78e5215b919501f0d1e21e
Giftos1 <giftsydney676@gmail.com>
2023-07-08 03:28:23 +1000

    initial commit

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -10,7 +10,7 @@ into the same case (lower or upper case) in order to check for palindromes.
 We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.*/
 
 function palindrome(str) {
-    return str
+    return str;
 }
 
 console.log(palindrome("eye"))
