0de650740198f39db3ce8cfa4bdab37aebaf709c
Giftos1 <giftsydney676@gmail.com>
2023-07-09 00:12:18 +1000

    return decoded string

--- CaesarsCipher.js
+++ CaesarsCipher.js
@@ -9,7 +9,24 @@ The function  takes a ROT13 encoded string as input and returns a decoded string
 All letters are uppercase. Non-alphabetic characters (i.e. spaces, punctuation), have been passed.*/
 
 function rot13(str) {
-    return str;
+    const strArr = []
+    const strRegex = /\W|_/g;
+
+    for (let i = 0; i < str.length; i++){
+        const cipher = 13;
+        let newChar = str.charCodeAt(i) - cipher;
+        let newStr = String.fromCharCode(newChar);
+        if (newChar >= 65 && !strRegex.test(str[i])) {
+            strArr.push(newStr);
+        } else if (newChar < 65 && !strRegex.test(str[i])){
+            let otherChar = 90 - (64 - newChar); // 90 is the charCode of Z
+            newStr = String.fromCharCode(otherChar);
+            strArr.push(newStr);
+        } else {
+            strArr.push(str[i]);
+        }
+    }
+    return strArr.join("");
 }
 console.log(rot13("SERR PBQR PNZC"));
 console.log(rot13("SERR CVMMN!"));
