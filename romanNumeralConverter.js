fbcd258851887960f3ca9141e90560904ec8e892
Giftos1 <giftsydney676@gmail.com>
2023-08-11 19:02:18 +1000

    convert number to roman numeral

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -18,7 +18,29 @@ I	            1
 */
 
 function convertToRoman(num) {
-    return num;
+    let romanEquivalents = [
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I']
+    ];
+    if (num === 0) {
+        return '';
+    }
+    for (let i = 0; i < romanEquivalents.length; i++) {
+        if (num >= romanEquivalents[i][0]) {
+            return romanEquivalents[i][1] + convertToRoman(num - romanEquivalents[i][0]);
+        }
+    }
 }
 
 console.log(convertToRoman(36));
