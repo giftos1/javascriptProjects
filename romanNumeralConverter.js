b207be81a07c8ebbbc135c70a48fb98813b38d3c
Giftos1 <giftsydney676@gmail.com>
2023-08-12 15:15:40 +1000

    include tests for the function

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -43,4 +43,31 @@ function convertToRoman(num) {
     }
 }
 
+// tests
 console.log(convertToRoman(36));
+console.log(convertToRoman(2));
+console.log(convertToRoman(3));
+console.log(convertToRoman(4));
+console.log(convertToRoman(5));
+console.log(convertToRoman(9));
+console.log(convertToRoman(12));
+console.log(convertToRoman(16));
+console.log(convertToRoman(29));
+console.log(convertToRoman(44));
+console.log(convertToRoman(45));
+console.log(convertToRoman(68));
+console.log(convertToRoman(83));
+console.log(convertToRoman(97));
+console.log(convertToRoman(99));
+console.log(convertToRoman(400));
+console.log(convertToRoman(500));
+console.log(convertToRoman(501));
+console.log(convertToRoman(649));
+console.log(convertToRoman(798));
+console.log(convertToRoman(891));
+console.log(convertToRoman(1000));
+console.log(convertToRoman(1004));
+console.log(convertToRoman(1006));
+console.log(convertToRoman(1023));
+console.log(convertToRoman(2014));
+console.log(convertToRoman(3999));
